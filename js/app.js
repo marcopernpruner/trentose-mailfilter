0906d8bed48443a27710212e100ba147d64f10bd
Marco Pernpruner <marco.pernpruner@gmail.com>
2015-12-01 16:56:03 +0100

    Prima versione esercizio

--- js/app.js
+++ js/app.js
@@ -16,8 +16,19 @@ var MailModel = {
     * Filters out messages in the "database" that match the spam rules.
     * @return an array of messages, excluding those that match the filter rules.
     */
-    filter : function(){
-      return [];
+    filter : function() {
+      flt_msg = [];
+      for (var i=0; i<this.messages.length; i++) {
+        spam = false;
+        for (var j=0; j<this.rules.length; j++) {
+          if (this.messages[i].search(this.rules[j]) != -1) {
+            spam = true;
+          }
+        }
+        if (!spam) flt_msg.push(this.messages[i]);
+      }
+      this.messages = flt_msg;
+      return this.messages;
     }
 
   
@@ -32,8 +43,32 @@ var MailModel = {
 // We suggest to use js patters. 
 // you can add here your views and controllers if you decide to do so.
 
+var View = {
+    init : function() {
+        $(".result").html("");
+        array = Controller.getMessages();
+        for (var k=0; k<array.length; k++) {
+            $(".result").append("<li>"+array[k]+"</li>");
+        }
+    }
+};
 
+var Controller = {
+    init : function() {
+        MailModel.init();
+        View.init();
+    },
+    
+    getMessages : function() {
+        return MailModel.messages;
+    }
+};
 
 $(document).ready(function(){
-
+    Controller.init();
+    
+    $(".btn-filter").on("click",function() {
+       MailModel.filter();
+       View.init();
+    });
 });
